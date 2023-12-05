3ab3dcf0b2be107435996586a3a9cd3ea563e18a
Tim Rutjens <535094@fhict.nl>
2023-12-05 13:16:19 +0100

    read csv file exc completed

---
+++ ReadFromFile/ReadFromFile/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ReadFromFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            openFileDialog1 = new OpenFileDialog();
+            button1 = new Button();
+            gbxFirstName = new GroupBox();
+            lbxFirstName = new ListBox();
+            gbxLastName = new GroupBox();
+            lbxLastName = new ListBox();
+            gbxEmail = new GroupBox();
+            lbxEmail = new ListBox();
+            gbxGender = new GroupBox();
+            lbxGender = new ListBox();
+            gbxIP = new GroupBox();
+            lbxIP = new ListBox();
+            gbxFirstName.SuspendLayout();
+            gbxLastName.SuspendLayout();
+            gbxEmail.SuspendLayout();
+            gbxGender.SuspendLayout();
+            gbxIP.SuspendLayout();
+            SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(190, 34);
+            button1.TabIndex = 0;
+            button1.Text = "Select a .csv file";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // gbxFirstName
+            // 
+            gbxFirstName.Controls.Add(lbxFirstName);
+            gbxFirstName.Location = new Point(12, 52);
+            gbxFirstName.Name = "gbxFirstName";
+            gbxFirstName.Size = new Size(220, 396);
+            gbxFirstName.TabIndex = 1;
+            gbxFirstName.TabStop = false;
+            gbxFirstName.Text = "First name";
+            // 
+            // lbxFirstName
+            // 
+            lbxFirstName.FormattingEnabled = true;
+            lbxFirstName.ItemHeight = 25;
+            lbxFirstName.Location = new Point(6, 30);
+            lbxFirstName.Name = "lbxFirstName";
+            lbxFirstName.Size = new Size(208, 354);
+            lbxFirstName.TabIndex = 1;
+            // 
+            // gbxLastName
+            // 
+            gbxLastName.Controls.Add(lbxLastName);
+            gbxLastName.Location = new Point(238, 52);
+            gbxLastName.Name = "gbxLastName";
+            gbxLastName.Size = new Size(220, 396);
+            gbxLastName.TabIndex = 2;
+            gbxLastName.TabStop = false;
+            gbxLastName.Text = "Last name";
+            // 
+            // lbxLastName
+            // 
+            lbxLastName.FormattingEnabled = true;
+            lbxLastName.ItemHeight = 25;
+            lbxLastName.Location = new Point(0, 30);
+            lbxLastName.Name = "lbxLastName";
+            lbxLastName.Size = new Size(208, 354);
+            lbxLastName.TabIndex = 2;
+            // 
+            // gbxEmail
+            // 
+            gbxEmail.Controls.Add(lbxEmail);
+            gbxEmail.Location = new Point(464, 52);
+            gbxEmail.Name = "gbxEmail";
+            gbxEmail.Size = new Size(286, 396);
+            gbxEmail.TabIndex = 2;
+            gbxEmail.TabStop = false;
+            gbxEmail.Text = "Email";
+            // 
+            // lbxEmail
+            // 
+            lbxEmail.FormattingEnabled = true;
+            lbxEmail.ItemHeight = 25;
+            lbxEmail.Location = new Point(6, 30);
+            lbxEmail.Name = "lbxEmail";
+            lbxEmail.Size = new Size(274, 354);
+            lbxEmail.TabIndex = 0;
+            // 
+            // gbxGender
+            // 
+            gbxGender.Controls.Add(lbxGender);
+            gbxGender.Location = new Point(756, 52);
+            gbxGender.Name = "gbxGender";
+            gbxGender.Size = new Size(154, 396);
+            gbxGender.TabIndex = 3;
+            gbxGender.TabStop = false;
+            gbxGender.Text = "Gender";
+            // 
+            // lbxGender
+            // 
+            lbxGender.FormattingEnabled = true;
+            lbxGender.ItemHeight = 25;
+            lbxGender.Location = new Point(6, 30);
+            lbxGender.Name = "lbxGender";
+            lbxGender.Size = new Size(142, 354);
+            lbxGender.TabIndex = 3;
+            // 
+            // gbxIP
+            // 
+            gbxIP.Controls.Add(lbxIP);
+            gbxIP.Location = new Point(916, 52);
+            gbxIP.Name = "gbxIP";
+            gbxIP.Size = new Size(220, 396);
+            gbxIP.TabIndex = 2;
+            gbxIP.TabStop = false;
+            gbxIP.Text = "IP-Adress";
+            // 
+            // lbxIP
+            // 
+            lbxIP.FormattingEnabled = true;
+            lbxIP.ItemHeight = 25;
+            lbxIP.Location = new Point(6, 30);
+            lbxIP.Name = "lbxIP";
+            lbxIP.Size = new Size(208, 354);
+            lbxIP.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1153, 471);
+            Controls.Add(gbxIP);
+            Controls.Add(gbxGender);
+            Controls.Add(gbxEmail);
+            Controls.Add(gbxLastName);
+            Controls.Add(gbxFirstName);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            gbxFirstName.ResumeLayout(false);
+            gbxLastName.ResumeLayout(false);
+            gbxEmail.ResumeLayout(false);
+            gbxGender.ResumeLayout(false);
+            gbxIP.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private Button button1;
+        private GroupBox gbxFirstName;
+        private GroupBox gbxLastName;
+        private GroupBox gbxEmail;
+        private GroupBox gbxGender;
+        private GroupBox gbxIP;
+        private ListBox lbxFirstName;
+        private ListBox lbxLastName;
+        private ListBox lbxEmail;
+        private ListBox lbxGender;
+        private ListBox lbxIP;
+    }
+}
---
+++ ReadFromFile/ReadFromFile/Form1.cs
@@ -0,0 +1,92 @@
+namespace ReadFromFile
+{
+    public partial class Form1 : Form
+    {
+        List<Person> list = new List<Person>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (openFileDialog1)
+            {
+                openFileDialog1.Filter = "CSV files (*.csv)|*.csv";
+                openFileDialog1.FilterIndex = 1;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog1.FileName;
+                    try
+                    {
+                        string[] csvLines = File.ReadAllLines(filePath);
+                        foreach (string line in csvLines)
+                        {
+                            try
+                            {
+                                Person person = new Person(line);
+                                list.Add(person);
+                            }
+                            catch (Exception)
+                            {
+                                MessageBox.Show("Error reading file");
+                            }
+                        }
+                        UpdateListBoxes();
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Error");
+                    }
+                }
+            }
+        }
+
+        private void UpdateListBoxes()
+        {
+            lbxFirstName.Items.Clear();
+            lbxLastName.Items.Clear();
+            lbxEmail.Items.Clear();
+            lbxGender.Items.Clear();
+            lbxIP.Items.Clear();
+
+            foreach (Person person in list)
+            {
+                lbxFirstName.Items.Add(person.First_name);
+                lbxLastName.Items.Add(person.Last_name);
+                lbxEmail.Items.Add(person.Email);
+                lbxGender.Items.Add(person.Gender);
+                lbxIP.Items.Add(person.Ip_adress);
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Slogan { get; set; }
+        public string Ip_adress { get; set; }
+
+        public Person(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            if (values.Length >= 6)
+            {
+                First_name = values[0];
+                Last_name = values[1];
+                Email = values[2];
+                Gender = values[3];
+                Slogan = values[4];
+                Ip_adress = values[5];
+            }
+            else { throw new Exception("Invalid CSV format."); }
+
+
+        }
+
+    }
+}
