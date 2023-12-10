9af5855431d8bc9fcde694034cad5174a5012b97
Tim Rutjens <535094@fhict.nl>
2023-12-10 22:13:57 +0100

    works except for playlist function, might add as extra

--- StreamingMusicApp/StreamingMusicApp/Form1.cs
+++ StreamingMusicApp/StreamingMusicApp/Form1.cs
@@ -1,8 +1,11 @@
+using System.Net;
+
 namespace StreamingMusicApp
 {
     public partial class Form1 : Form
     {
         User user;
+        StreamingMusicService service = new StreamingMusicService("service"); 
         public Form1()
         {
             InitializeComponent();
@@ -33,14 +36,73 @@ namespace StreamingMusicApp
             lblLoggedInUser.Text = $"Logged in user: {userEmail}";
         }
 
+        private void RefreshQueue()
+        {
+            Song[] allSongs = service.GetSongs();
+
+            clbxSongsQueue.Items.Clear();
+
+            foreach (Song song in allSongs)
+            {
+                clbxSongsQueue.Items.Add(song.GetInfo()); 
+            }
+        }
+
+        private void RefreshFavorites() 
+        {
+            Song[] allSongs = service.GetSongs();
+
+            clbxFavoritesList.Items.Clear();
+
+            foreach (Song song in allSongs) 
+            {
+                clbxFavoritesList.Items.Add(song.GetInfo());
+            }
+        }
+
+
         private void btnSongsAddQueue_Click(object sender, EventArgs e)
         {
-            
+            string artist = tbxSongsArtist.Text;
+            string title = tbxSongsTitle.Text;
+            Genre genre;
+            if (!Enum.TryParse(cbxSongsGenre.SelectedItem.ToString(), out genre)) 
+            {
+                MessageBox.Show("Invalid genre, select one that is provided.");
+            }
+            int durationInSeconds = (int)numSongsDuration.Value;
+
+            if (string.IsNullOrWhiteSpace(artist) || string.IsNullOrWhiteSpace(title) || (numSongsDuration.Value == 0))
+            {
+                MessageBox.Show("Please fill in all the song details.");
+                return;
+            }
+
+            service.AddSong(artist, title, genre, durationInSeconds);
+
+            MessageBox.Show($"Song added to the music service: {artist} - {title}");
+            RefreshQueue();
         }
+
 
         private void btnSongsAddFavorites_Click(object sender, EventArgs e)
         {
+            string artist = tbxSongsArtist.Text;
+            string title = tbxSongsTitle.Text;
+            Genre genre;
+            if (!Enum.TryParse(cbxSongsGenre.SelectedItem.ToString(), out genre))
+            {
+                MessageBox.Show("Invalid genre, select one that is provided.");
+            }
+            int durationInSeconds = (int)numSongsDuration.Value;
 
+            if (string.IsNullOrWhiteSpace(artist) || string.IsNullOrWhiteSpace(title) || (numSongsDuration.Value == 0))
+            {
+                MessageBox.Show("Please fill in all the song details.");
+                return;
+            }
+            MessageBox.Show("Song added to favorites.");
+            RefreshFavorites();
         }
 
         private void btnSongsAddPlaylist_Click(object sender, EventArgs e)
@@ -50,19 +112,60 @@ namespace StreamingMusicApp
 
         private void btnSongsRemoveQueue_Click(object sender, EventArgs e)
         {
+            if (clbxSongsQueue.CheckedItems.Count > 0)
+            {
+                object[] checkedItemsCopy = new object[clbxSongsQueue.CheckedItems.Count];
+                clbxSongsQueue.CheckedItems.CopyTo(checkedItemsCopy, 0);
 
+                foreach (object checkedItem in checkedItemsCopy)
+                {
+                    clbxSongsQueue.Items.Remove(checkedItem);
+                }
+            }
         }
+
 
         private void btnSongsQueueAddFavorites_Click(object sender, EventArgs e)
         {
+            if (clbxSongsQueue.CheckedItems.Count > 0)
+            {
+                foreach (object checkedItem in clbxSongsQueue.CheckedItems)
+                {
+                    string songInfo = checkedItem.ToString();
+                    int songID = int.Parse(songInfo.Substring(songInfo.IndexOf("ID")));
 
+                    Song selectedSong = service.GetSong(songID);
+
+                    if (selectedSong != null)
+                    {
+                        user.AddSongToFavorites(selectedSong);
+                        clbxFavoritesList.Items.Add(selectedSong);
+                    }
+                }
+            }
+            MessageBox.Show("Song added to favorites.");
         }
 
         private void btnRemoveFavorites_Click(object sender, EventArgs e)
         {
+            if (clbxFavoritesList.CheckedItems.Count > 0)
+            {
+                object[] checkedItemsCopy = new object[clbxFavoritesList.CheckedItems.Count];
+                clbxFavoritesList.CheckedItems.CopyTo(checkedItemsCopy, 0);
 
+                foreach (object checkedItem in checkedItemsCopy)
+                {
+                    clbxFavoritesList.Items.Remove(checkedItem);
+
+                    if (checkedItem is Song selectedSong)
+                    {
+                        user.RemoveSongFromFavorites(selectedSong);
+                    }
+                }
+            }
         }
 
-        
+
+
     }
 }
